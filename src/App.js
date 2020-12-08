842a683f499bb01eff814843bba1159e00f8f843
Nathan Langridge <nathanlangridge@hotmail.com>
2020-12-08 14:34:05 +0000

    flight number, name, year, pulled from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://api.spacexdata.com/v3/launches')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+
+  }
+
+  render() {
+
+    const { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
+    else {
+
+      return (
+        <div className="App">
+          <ul>
+            {items.map(item => (
+              <li key={item.flight_number}>
+                Flight ID: {item.flight_number}
+                Mission Name: {item.mission_name}
+                Launch Year: {item.launch_year}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+
+    }
+
+    }
+
 }
 
 export default App;
